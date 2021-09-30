7bb401cb4ad5b7732cbbc50555f3b2dcb4998134
Ibrahim  Andraws <55102557+Iandraws@users.noreply.github.com>
2021-09-30 20:22:56 +0200

    refactor

--- src/Input.js
+++ src/Input.js
@@ -7,21 +7,21 @@ import CardContainer from './CardContainer';
 export default function Input() {
 	const [InputText, setInputText] = useState('');
 	const [data, setData] = useState('');
-	const [showdata, setshowdata] = useState(false)
+	const [showdata, setshowdata] = useState(false);
 	const handleSubmit = (evt) => {
 		evt.preventDefault();
 		console.log(data);
-		setshowdata(true)
-		console.log(showdata)
+		setshowdata(true);
+		console.log(showdata);
 	};
-	useEffect(() => {
-		const fetchData = async () => {
-			const result = await axios(
-				`http://api.openweathermap.org/data/2.5/weather?q=${InputText}&appid=e64017f055c2c4169cf6910ef8abb16c`
-			);
+	const fetchData = async () => {
+		const result = await axios(
+			`http://api.openweathermap.org/data/2.5/weather?q=${InputText}&appid=e64017f055c2c4169cf6910ef8abb16c`
+		);
+		setData(result.data);
+	};
 
-			setData(result.data);
-		};
+	useEffect(() => {
 		fetchData();
 	}, [InputText]);
 
@@ -44,14 +44,7 @@ export default function Input() {
 					/>
 				</Box>
 			</form>
-			{showdata?(<CardContainer data={data}/>)
-			:(<div>
-				Please enter city
-			</div>)
-			
-}
-
-
+			{showdata ? <CardContainer data={data} /> : <div>Please enter city</div>}
 		</div>
 	);
 }
